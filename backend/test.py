b8ee3a5cf3ef47439ba8fa9cb9757985e423a5a9
SnowSword1010 <jmayank2001@gmail.com>
2022-10-12 14:22:51 +0530

    :sparkles: Application caters to image inputs

--- backend/test.py
+++ backend/test.py
@@ -1,3 +1,4 @@
+from unittest import result
 import cv2
 import mediapipe as mp
 # Import the required module for text 
@@ -8,77 +9,60 @@ from gtts import gTTS
 # play the converted audio
 import os
 from playsound import playsound
-import pygame
+# import pygame
 
 
 
-# pygame.quit()
+def alert():
+  playsound('./audios/no_input.wav')
+
+def voiceover(palm):
+  if(palm == 'Left'):
+    playsound('./audios/left_palm.wav')
+  elif(palm == 'Right'):
+    playsound('./audios/right_palm.wav')
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-print('hello')
-import threading
-def alert():
-  pass
-    # pygame.init()
-    # pygame.mixer.music.load('welcome.wav')
-    # pygame.mixer.music.play()
-    # threading.Thread(target=playsound, args=('welcome.wav',), daemon=True).start()
-
-# For webcam input:
-cap = cv2.VideoCapture(0)
+# For static images:
+IMAGE_FILES = [r'./pictures/left_hand.jpg']
 with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
+    static_image_mode=True,
+    max_num_hands=2,
+    min_detection_confidence=0.5) as hands:
+  for idx, file in enumerate(IMAGE_FILES):
+    # Read an image, flip it around y-axis for correct handedness output (see
+    # above).
+    image = cv2.flip(cv2.imread(file), 1)
+    # Convert the BGR image to RGB before processing.
+    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+    # Print handedness and draw hand landmarks on the image.
+    print('Handedness:', results.multi_handedness)
+    if not results.multi_hand_landmarks:
       continue
-
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
-
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+    image_height, image_width, _ = image.shape
+    annotated_image = image.copy()
     ans = results.multi_handedness
     if(ans == None):
-      # # The text that you want to convert to audio
-      # mytext = 'Hand input not received'
-      # # Language in which you want to convert
-      # language = 'en'
-      # # Passing the text and language to the engine, 
-      # # here we have marked slow=False. Which tells 
-      # # the module that the converted audio should 
-      # # have a high speed
-      # myobj = gTTS(text=mytext, lang=language, slow=False)
-      # # Saving the converted audio in a mp3 file named
-      # # welcome 
-      # myobj.save("welcome.mp3")
-      # # Playing the converted file
-      # os.system("mpg321 welcome.mp3")
       alert()
     else:
-      print(ans[0].classification[0].label)
-    # print(results.multi_handedness)
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
-cap.release()
+      voiceover(ans[0].classification[0].label)
+
+    for hand_landmarks in results.multi_hand_landmarks:
+      mp_drawing.draw_landmarks(
+          annotated_image,
+          hand_landmarks,
+          mp_hands.HAND_CONNECTIONS,
+          mp_drawing_styles.get_default_hand_landmarks_style(),
+          mp_drawing_styles.get_default_hand_connections_style())
+    cv2.imwrite(
+        '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
+    # Draw hand world landmarks.
+    # if not results.multi_hand_world_landmarks:
+    #   continue
+    # for hand_world_landmarks in results.multi_hand_world_landmarks:
+    #   mp_drawing.plot_landmarks(
+    #     hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
