4158e3ff64e9991d7d750d4c8e97402ef6ffcc30
SnowSword1010 <jmayank2001@gmail.com>
2022-10-13 22:47:03 +0530

    :hammer: Modularised code | Added flask support

--- backend/test.py
+++ backend/test.py
@@ -3,7 +3,7 @@ import cv2
 import mediapipe as mp
 # Import the required module for text 
 # to speech conversion
-from gtts import gTTS
+# from gtts import gTTS
 
 # This module is imported so that we can 
 # play the converted audio
@@ -22,47 +22,49 @@ def voiceover(palm):
   elif(palm == 'Right'):
     playsound('./audios/right_palm.wav')
 
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_hands = mp.solutions.hands
+def scan_image(img):
+  mp_drawing = mp.solutions.drawing_utils
+  mp_drawing_styles = mp.solutions.drawing_styles
+  mp_hands = mp.solutions.hands
 
-# For static images:
-IMAGE_FILES = [r'./pictures/left_hand.jpg']
-with mp_hands.Hands(
-    static_image_mode=True,
-    max_num_hands=2,
-    min_detection_confidence=0.5) as hands:
-  for idx, file in enumerate(IMAGE_FILES):
-    # Read an image, flip it around y-axis for correct handedness output (see
-    # above).
-    image = cv2.flip(cv2.imread(file), 1)
-    # Convert the BGR image to RGB before processing.
-    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+  # For static images:
+  IMAGE_FILES = [path]
+  with mp_hands.Hands(
+      static_image_mode=True,
+      max_num_hands=2,
+      min_detection_confidence=0.5) as hands:
+    for idx, file in enumerate(IMAGE_FILES):
+      # Read an image, flip it around y-axis for correct handedness output (see
+      # above).
+      # image = cv2.flip(cv2.imread(file), 1)
+      image = cv2.flip(img, 1)
+      # Convert the BGR image to RGB before processing.
+      results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-    # Print handedness and draw hand landmarks on the image.
-    print('Handedness:', results.multi_handedness)
-    if not results.multi_hand_landmarks:
-      continue
-    image_height, image_width, _ = image.shape
-    annotated_image = image.copy()
-    ans = results.multi_handedness
-    if(ans == None):
-      alert()
-    else:
-      voiceover(ans[0].classification[0].label)
+      # Print handedness and draw hand landmarks on the image.
+      print('Handedness:', results.multi_handedness)
+      if not results.multi_hand_landmarks:
+        continue
+      image_height, image_width, _ = image.shape
+      annotated_image = image.copy()
+      ans = results.multi_handedness
+      if(ans == None):
+        alert()
+      else:
+        voiceover(ans[0].classification[0].label)
 
-    for hand_landmarks in results.multi_hand_landmarks:
-      mp_drawing.draw_landmarks(
-          annotated_image,
-          hand_landmarks,
-          mp_hands.HAND_CONNECTIONS,
-          mp_drawing_styles.get_default_hand_landmarks_style(),
-          mp_drawing_styles.get_default_hand_connections_style())
-    cv2.imwrite(
-        '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
-    # Draw hand world landmarks.
-    # if not results.multi_hand_world_landmarks:
-    #   continue
-    # for hand_world_landmarks in results.multi_hand_world_landmarks:
-    #   mp_drawing.plot_landmarks(
-    #     hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
+      for hand_landmarks in results.multi_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            annotated_image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
+      cv2.imwrite(
+          '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
+      # Draw hand world landmarks.
+      # if not results.multi_hand_world_landmarks:
+      #   continue
+      # for hand_world_landmarks in results.multi_hand_world_landmarks:
+      #   mp_drawing.plot_landmarks(
+      #     hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
