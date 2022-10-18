d9f53dc7ac02df146a1422c74a3ae1c6ed978974
SnowSword1010 <jmayank2001@gmail.com>
2022-10-18 20:23:36 +0530

    :sparkles: Added support for palm and back detection

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+import cv2
 import test
 
 app = Flask(__name__)
 @app.route('/scan', methods = ['POST'])
 def scan():
-    img = request.form.get('inputImage')
-    test.scan_image(img)
-# test.scan_image('./pictures/left_hand.jpg')
+    img = cv2.imread('./pictures/left_hand.jpg')
+    return test.scan_image(cv2.imread('./pictures/left_hand.jpg'))
+    # img = request.form.get('inputImage')
+    # test.scan_image(img)
+
+print(test.scan_image(cv2.imread('./pictures/left_hand.jpg')))
--- backend/test.py
+++ backend/test.py
@@ -14,13 +14,23 @@ from playsound import playsound
 
 
 def alert():
-  playsound('./audios/no_input.wav')
+  playsound('./audios/np_input.wav')
 
-def voiceover(palm):
+def voiceover(palm, side):
   if(palm == 'Left'):
-    playsound('./audios/left_palm.wav')
-  elif(palm == 'Right'):
-    playsound('./audios/right_palm.wav')
+    if(side == "palm"):
+      return 1
+    else:
+      return 2
+  else:
+    if(side == "palm"):
+      return 3
+    else:
+      return 4
+  
+    # playsound('./audios/left_palm.wav')
+  # elif(palm == 'Right'):
+    # playsound('./audios/right_palm.wav')
 
 def scan_image(img):
   mp_drawing = mp.solutions.drawing_utils
@@ -28,7 +38,7 @@ def scan_image(img):
   mp_hands = mp.solutions.hands
 
   # For static images:
-  IMAGE_FILES = [path]
+  IMAGE_FILES = [img]
   with mp_hands.Hands(
       static_image_mode=True,
       max_num_hands=2,
@@ -48,23 +58,41 @@ def scan_image(img):
       image_height, image_width, _ = image.shape
       annotated_image = image.copy()
       ans = results.multi_handedness
+      output = 0
       if(ans == None):
-        alert()
+        output = 0
+        # alert()
       else:
-        voiceover(ans[0].classification[0].label)
-
-      for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            annotated_image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
-      cv2.imwrite(
-          '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
+        thumb_x = results.multi_hand_landmarks[0].landmark[4].x
+        pinky_x = results.multi_hand_landmarks[0].landmark[20].x
+        side = "back"
+        if(thumb_x > pinky_x):
+          side = "palm"
+        
+        output = voiceover(ans[0].classification[0].label, side)
+        # print("palm")
+        # else:
+          # print("back")
+        
+        # output = str(ans[0].classification[0].label) + " " + str(side) + " detected"
+        print(output)
+        # return output
+        # voiceover(ans[0].classification[0].label)
+        return output
+      # for hand_landmarks in results.multi_hand_landmarks:
+      #   mp_drawing.draw_landmarks(
+      #       annotated_image,
+      #       hand_landmarks,
+      #       mp_hands.HAND_CONNECTIONS,
+      #       mp_drawing_styles.get_default_hand_landmarks_style(),
+      #       mp_drawing_styles.get_default_hand_connections_style())
+      # cv2.imwrite(
+      #     '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
       # Draw hand world landmarks.
       # if not results.multi_hand_world_landmarks:
       #   continue
       # for hand_world_landmarks in results.multi_hand_world_landmarks:
       #   mp_drawing.plot_landmarks(
       #     hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
+
+# scan_image(cv2.imread('./pictures/left_hand.jpg'))
